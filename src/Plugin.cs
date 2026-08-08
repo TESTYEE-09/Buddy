4ca0229adef4ffd67e6522b56d90a370d9af6efc
TESTYEE-09 <86476589+TESTYEE-09@users.noreply.github.com>
2026-08-08 20:39:48 +1000

    Route realtime model through Responses API

--- src/Plugin.cs
+++ src/Plugin.cs
@@ -12,7 +12,7 @@ namespace LethalAICrewmate
     {
         public const string ModGuid = "com.lethalaicrewmate.buddy";
         public const string ModName = "LethalAICrewmate";
-        public const string ModVersion = "1.6.2";
+        public const string ModVersion = "1.6.3";
 
         internal static Plugin Instance;
         internal static ManualLogSource Log;
